8e12be080ce5e20bf977dec6f657f932e54db164
Prajwal Korban <128471316+prajjukorban@users.noreply.github.com>
2025-10-02 14:24:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 
 genai.configure(api_key="AIzaSyBMl0IeB6js1nuAFE3Dtjzn1eFN__LCfTs")
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 st.set_page_config(page_title="Resume Career Guide", layout="centered")
 st.title("📄 AI Career Copilot")
